d5a1b3c39189472424f243cc30b5914fc8c25706
Choi-R <choirul.rahmaditya@mail.ugm.ac.id>
2020-01-21 11:48:30 +0700

    [Choi]Read lagi euy

--- read.js
+++ read.js
@@ -2,7 +2,8 @@ function readUser(ID){
     var users = require('./data/users.json');
     for (let i=0; i< users.length;i++){
         if (ID == users[i]['id']){ 
-            users = users[i]
+            users = users[i];
+            delete users['password'];
         }
     } 
     return users;
