45463cd7674f1694e96482678bcc6968e8aa54fa
ellvisca <andrio.kevin12@gmail.com>
2020-01-22 12:00:43 +0700

    [Kevin] Create Promise

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+
 var create = require('./create.js');
+var read = require('./read.js');
+//var update = require('./update.js');
+//var Delete = require('./delete.js');
 var args = process.argv.slice(2);
 
 switch(args[0]) {
@@ -6,9 +10,15 @@ switch(args[0]) {
     var name = args[1];
     var email = args[2];
     var password = args[3];
-    
-    create.user(name, email, password);
+
+    create.user({name, email, password}).then(data => {
+      console.log(data);
+    })
+    .catch(err => {
+    console.error(err);
+    });
     break;
+  
   case 'read_user':
 
     break;
@@ -24,3 +34,49 @@ switch(args[0]) {
   default:
     console.log('Unknown operation!')
 }
+
+
+/*var isBanana = function(data) {
+  return new Promise (function(resolve,reject) {
+    if (data === "Banana") {
+
+      resolve ("Yes");
+    } else{
+      reject("No");
+    }
+  })
+}
+
+isBanana("Bukan Banana").then(data => {
+  console.log(data);
+})
+.catch(err => {
+console.error(err);
+});
+
+console.log('Uy uy');
+*/
+
+/*
+let isTrue = true;
+let promise = new Promise(function(resolve, reject){
+  if (isTrue) {
+    resolve("Hello World");
+  }
+  else {
+    reject("Bye World");
+  }
+})
+
+async function jalanin() {
+  try {
+    let result = await promise;
+    console.log(result);
+  }
+  catch(err){
+    console.log(err);
+  }
+}
+
+jalanin();
+*/
